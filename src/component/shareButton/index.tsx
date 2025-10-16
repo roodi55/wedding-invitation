d0b7dde10ae61ca2486539e9c0611ab019e291b0
sjlee22 <sjlee@itrs.co.kr>
2025-10-16 22:23:50 +0900

    update share

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -38,7 +38,7 @@ export const ShareButton = () => {
                 "//" +
                 window.location.host +
                 baseUrl +
-                "/preview_image.png",
+                "preview_image.png",
               link: {
                 mobileWebUrl:
                   window.location.protocol +
